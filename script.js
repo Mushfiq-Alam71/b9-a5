7aa6550a0b0ef663cd7b999ef2279df7d280da84
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-02-19 16:07:40 +0600

    added js for removing selected items

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const seat = document.getElementsByClassName("busSeat");
 const seatColor = new Array(seat.length).fill(false);
 const availableSeat = document.getElementById("availableSeat");
 let availableSeatValue = availableSeat.innerText;
+
 let seatNumber = document.getElementById("seat-number");
 let seatClass = document.getElementById("seat-class");
 let seatPrice = document.getElementById("seat-price");
+
 let seatCountNumber = document.getElementById("seat-count-number");
 
 let totalPrice = document.getElementById("total-price");
 let discountPrice = document.getElementById("discount-price");
 let selectSeatCount = 0;
 let price = 0;
+
+let busSeatArrangement = [];
 
 for (let i = 0; i < seat.length; i++) {
   seat[i].addEventListener("click", function () {
@@ -22,6 +26,19 @@ for (let i = 0; i < seat.length; i++) {
       availableSeat.innerHTML = availableSeatValue;
       selectSeatCount--;
       seatCountNumber.innerHTML = selectSeatCount;
+
+      let seatIndex = busSeatArrangement.indexOf(seat[i].innerText);
+      seatNumber.removeChild(seatNumber.children[seatIndex]);
+      seatClass.removeChild(seatClass.children[seatIndex]);
+      seatPrice.removeChild(seatPrice.children[seatIndex]);
+
+      price = selectSeatCount * 550;
+      totalPrice.innerHTML = price;
+      discountPrice.innerHTML = price;
+
+      busSeatArrangement = busSeatArrangement.filter(
+        (seats) => seats != seat[i].innerText
+      );
     } else {
       if (selectSeatCount < 4) {
         seat[i].style.backgroundColor = "#1DD100";
@@ -49,6 +66,8 @@ for (let i = 0; i < seat.length; i++) {
         price = selectSeatCount * 550;
         totalPrice.innerHTML = price;
         discountPrice.innerHTML = price;
+
+        busSeatArrangement.push(seat[i].innerText);
       }
     }
   });
